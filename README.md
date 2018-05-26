b1b5152b1440ec14cf881bf860c70c51f38bdb60
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2018-05-25 21:31:45 -0400

    Add patch output mode

--- README.md
+++ README.md
@@ -13,7 +13,22 @@ This adds the `riptr` and `rtr` cli tools to you python environment path.
 # Usage
 ```bash
 echo "some example text 1234" > test.file
+
+# default output is stdout
 riptr -m "^(.*?)([\d]{2})" -s "\g<1>56" test.file
+some example text 5634
+
+# specify patch mode 'p'
+riptr -m "^(.*?)([\d]{2})" -s "\g<1>56" -o p test.file
+---
++++
+@@ -1 +1 @@
+-some example text 1234
++some example text 5634
+
+# or inplace
+riptr -m "^(.*?)([\d]{2})" -s "\g<1>56" -o i test.file
+cat test.file
 some example text 5634
 
 ```
--- riptr.py
+++ riptr.py
@@ -4,31 +4,84 @@ RIP tr/sed. Python regex replacement utility.
 """
 from __future__ import print_function
 import argparse
+import difflib
 import re
 
-__version__ = '1.0.3'
+__version__ = "1.1.0"
+
+
+def output_unified_diff(olddata, newdata, oldfile):
+    """Output unified diff from old->new"""
+    del oldfile
+    print(
+        "\n".join(
+            difflib.unified_diff(
+                olddata.splitlines(), newdata.splitlines(), lineterm=""
+            )
+        )
+    )
+
+
+def output_stdout(olddata, newdata, oldfile):
+    """Output result to stdout"""
+    del olddata, oldfile
+    print(newdata)
+
+
+def output_inplace(olddata, newdata, oldfile):
+    """Output result to file-like"""
+    del olddata
+    with open(oldfile, "w") as outfile:
+        outfile.write(newdata)
 
 
 def main():
     """Run some regex substitutions on cmd line files"""
     parser = argparse.ArgumentParser(
-        description='Rip tr v{VERSION}'.format(VERSION=__version__))
-    parser.add_argument('file', metavar='FILE', type=str, nargs='+',
-                        help=('Input file(s). Combine with -w to speed this up, eg "riptr '
-                              '-m <match> -s <sub> -w $(ls <dir>)"'))
-    parser.add_argument('-m', '--match', metavar='REGEX', type=str,
-                        help='Matching regex, python syntax', required=True)
-    parser.add_argument('-s', '--substitute', metavar='REGEX', type=str,
-                        help='Substitute regex, python syntax; backref groups per the --match arg.',
-                        required=True)
-    parser.add_argument('-i', '--inplace', action="store_true",
-                        help="Edit files in place, instead of emitting to stdout")
-    parser.add_argument('-d', '--dotall', action="store_true",
-                        help="Set dotall on the regex")
-    parser.add_argument('-l', '--multiline', action="store_true",
-                        help="Set multiline on the regex")
-    parser.add_argument('-v', '--version', action="store_true",
-                        help="Print version and exit")
+        description="Rip tr v{VERSION}".format(VERSION=__version__)
+    )
+    parser.add_argument(
+        "file",
+        metavar="FILE",
+        type=str,
+        nargs="+",
+        help=(
+            'Input file(s). Combine with -w to speed this up, eg "riptr '
+            '-m <match> -s <sub> -w $(ls <dir>)"'
+        ),
+    )
+    parser.add_argument(
+        "-m",
+        "--match",
+        metavar="REGEX",
+        type=str,
+        help="Matching regex, python syntax",
+        required=True,
+    )
+    parser.add_argument(
+        "-s",
+        "--substitute",
+        metavar="REGEX",
+        type=str,
+        help="Substitute regex, python syntax; backref groups per the --match arg.",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--outputmode",
+        choices=["inplace", "i", "patch", "p", "stdout", "s"],
+        help="Set output mode- inplace/i = edit in place, patch/p = generate unified patch",
+        default="stdout",
+    )
+    parser.add_argument(
+        "-d", "--dotall", action="store_true", help="Set dotall on the regex"
+    )
+    parser.add_argument(
+        "-l", "--multiline", action="store_true", help="Set multiline on the regex"
+    )
+    parser.add_argument(
+        "-v", "--version", action="store_true", help="Print version and exit"
+    )
     args = parser.parse_args()
 
     flags = 0
@@ -40,15 +93,22 @@ def main():
     matcher = re.compile(args.match, flags=flags)
 
     for fname in args.file:
-        with open(fname, 'r') as infile:
-            data = infile.read()
-        data = matcher.sub(args.substitute, data)
-        if args.inplace:
-            with open(fname, 'w') as outfile:
-                outfile.write(data)
-        else:
-            print(data)
+        with open(fname, "r") as infile:
+            indata = infile.read()
+        outdata = matcher.sub(args.substitute, indata)
+
+        # Map of outputters
+        outputters = {
+            "inplace": output_inplace,
+            "i": output_inplace,
+            "patch": output_unified_diff,
+            "p": output_unified_diff,
+            "stdout": output_stdout,
+            "s": output_stdout,
+        }
+        # Output result
+        outputters[args.outputmode](indata, outdata, fname)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
